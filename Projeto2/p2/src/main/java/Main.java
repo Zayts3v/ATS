53f5a6d0c94d3a3dc90a2bbe1423d9b25c5d8beb
Zayts3v <tiago-pinha@hotmail.com>
2020-01-16 18:40:14 +0000

    Adicionar projeto para o evosuite.

---
+++ Projeto2/p2/src/main/java/Main.java
@@ -0,0 +1,28 @@
+import controller.Controller;
+import model.Parser;
+import model.UMCarroJa;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+
+public class Main {
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        UMCarroJa model = new UMCarroJa();
+        try {
+            model = UMCarroJa.read(".tmp");
+        }
+        catch (IOException | ClassNotFoundException e) {
+            new Parser("db/logsPOO_carregamentoInicial.bak", model);
+        }
+        try { Thread.sleep(10000);} catch (Exception e) { logger.warning(e.toString()); }
+        new Controller(model).run();
+        try {
+            model.save(".tmp");
+        }
+        catch (IOException ignored) { logger.warning("Not saved!"); }
+    }
+}
